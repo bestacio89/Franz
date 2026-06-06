e73fdabf199d9504280b8d09f4614814cd4e0dd2
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-06-06 21:48:17 +0200

    [Refactor][Persistence]: convert existing seeders to implement isceder

--- sources/back/Franz.Persistence/Persistence/Seeders/MemberSeeder.cs
+++ sources/back/Franz.Persistence/Persistence/Seeders/MemberSeeder.cs
@@ -1,54 +1,39 @@
-﻿using Franz.Domain.Entities;
+﻿using System.Linq;
+using Franz.Common.Data;
+using Franz.Domain.Entities;
 using Franz.Domain.ValueObjects;
 
-namespace Franz.Persistence.Seeders
+namespace Franz.Persistence.Seeders;
+
+public sealed class MemberSeeder : ISeeder
 {
-    public static class MemberSeeder
+  private readonly ApplicationDbContext _db;
+
+  // Dependent data runs next
+  public int Order => 2;
+
+  public MemberSeeder(ApplicationDbContext db)
+  {
+    _db = db;
+  }
+
+  public async Task  SeedAsync(CancellationToken cancellation)
+  {
+    if (_db.Members.Any())
     {
-        public static void Seed(ApplicationDbContext db)
-        {
-            // Delete existing records
-            if (db.Members.Any())
-            {
-                db.Members.RemoveRange(db.Members);
-                db.SaveChanges();
-            }
-
-            // Add new records
-            db.Members.AddRange(
-                new Member(new FullName("John Doe"), new Email("john.doe@example.com"))
-                {
-                   
-                  
-                },
-                new Member(new FullName("Jane Smith"), new Email("jane.smith@example.com"))
-                {
-                    
-                  
-                },
-                new Member(new FullName("Alice Johnson"), new Email("alice.j@example.com"))
-                {
-                    
-                 
-                },
-                new Member(new FullName("Bob Roberts"), new Email("bob.r@example.com"))
-                {
-                   
-                   
-                },
-                new Member(new FullName("Charlie Brown"), new Email("charlie.brown@example.com"))
-                {
-                   
-                    
-                },
-                new Member(new FullName("Diana Prince"), new Email("diana.prince@example.com"))
-                {
-                  
-                   
-                }
-            );
-
-            db.SaveChanges();
-        }
+      await  _db.Members.AddRangeAsync(_db.Members);
+      await _db.SaveChangesAsync();
     }
+
+      await _db.Members.AddRangeAsync(
+        new Member(new FullName("John Doe"), new Email("john.doe@example.com")),
+        new Member(new FullName("Jane Smith"), new Email("jane.smith@example.com")),
+        new Member(new FullName("Alice Johnson"), new Email("alice.j@example.com")),
+        new Member(new FullName("Bob Roberts"), new Email("bob.r@example.com")),
+        new Member(new FullName("Charlie Brown"), new Email("charlie.brown@example.com")),
+        new Member(new FullName("Diana Prince"), new Email("diana.prince@example.com"))
+    );
+
+    await _db.SaveChangesAsync();
+  }
 }
