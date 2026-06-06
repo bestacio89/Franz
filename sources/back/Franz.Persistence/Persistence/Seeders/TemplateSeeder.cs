d3c577d26df9f5c7ca0e16699d2831ac421b5c61
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-06-06 21:48:17 +0200

    [Feat][Persistence]: introduce isceder interface and templatedatabaseseeder for managed data seeding

---
+++ sources/back/Franz.Persistence/Persistence/Seeders/TemplateSeeder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Franz.Common.Data;
+
+namespace Franz.Persistence;
+
+public static class TemplateDatabaseSeeder
+{
+  /// <summary>
+  /// Executes all self-discovered ISeeder implementations sequentially based on their defined priority.
+  /// </summary>
+  public static void Run(IServiceProvider serviceProvider)
+  {
+    var db = serviceProvider.GetRequiredService<ApplicationDbContext>();
+
+    // Relational schema migration safe-check
+    db.Database.Migrate();
+
+    // Self-discovered array resolution
+    var seeders = serviceProvider.GetServices<ISeeder>();
+
+    foreach (var seeder in seeders.OrderBy(s => s.Order))
+    {
+      seeder.SeedAsync();
+    }
+  }
+}
